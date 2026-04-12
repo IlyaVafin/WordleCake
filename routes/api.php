562390be22d4a28a965a0ece09f266c79a516624
IlyaVafin <rthrthrer5gh@gmail.com>
2026-04-12 10:53:05 +0300

    feat: #14 change auth

--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,25 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/users", [UserController::class, 'index']);
+Route::get("/users", [UserController::class, 'index'])->middleware('auth:sanctum');
 
 Route::get('/category', [CategoryController::class, 'index']);
 Route::get("/categories", [CategoryController::class, "getCategories"]);
-Route::post('/category', [CategoryController::class, 'store'])->middleware(['check-auth', 'admin']);
-Route::patch("/category/{id}", [CategoryController::class, "update"])->middleware(['check-auth', 'admin']);
-Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->middleware(['check-auth', 'admin']);
+Route::post('/category', [CategoryController::class, 'store'])->middleware(['auth:sanctum', 'admin']);
+Route::patch("/category/{id}", [CategoryController::class, "update"])->middleware(['auth:sanctum', 'admin']);
+Route::delete('/category/{id}', [CategoryController::class, 'destroy'])->middleware(['auth:sanctum', 'admin']);
 
-Route::get("/games/{game_uuid}/info", [GameSessionController::class, 'show'])->middleware('check-auth');
+Route::get("/games/{game_uuid}/info", [GameSessionController::class, 'show'])->middleware('auth:sanctum');
 Route::get("/games", [GameController::class, 'index']);
 Route::get("/games/{category_id}", [GameController::class, 'show']);
-Route::post("/game", [GameController::class, 'store'])->middleware(['check-auth', 'admin']);
-Route::post("/games/{game_id}", [GameSessionController::class, 'store'])->middleware(['check-auth']);
-Route::patch("/game/{id}", [GameController::class, 'update'])->middleware(['check-auth', 'admin']);
-Route::post("/games/{game_uuid}/answer", [AttemptController::class, "store"])->middleware(['check-auth']);
-Route::delete("/game/{game_id}", [GameController::class, 'destroy'])->middleware(['check-auth', 'admin']);
-Route::delete("/logout", [AuthController::class, 'logout'])->middleware(['check-auth']);
+Route::post("/game", [GameController::class, 'store'])->middleware(['auth:sanctum', 'admin']);
+Route::post("/games/{game_id}", [GameSessionController::class, 'store'])->middleware("auth:sanctum");
+Route::patch("/game/{id}", [GameController::class, 'update'])->middleware(['auth:sanctum', 'admin']);
+Route::post("/games/{game_uuid}/answer", [AttemptController::class, "store"])->middleware(['auth:sanctum']);
+Route::delete("/game/{game_id}", [GameController::class, 'destroy'])->middleware(['auth:sanctum', 'admin']);
+Route::delete("/logout", [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
 
-Route::get("/profile", [ProfileController::class, 'index']);
-Route::patch("/profile", [ProfileController::class, 'update']);
+Route::get("/profile", [ProfileController::class, 'index'])->middleware(['auth:sanctum']);
+Route::patch("/profile", [ProfileController::class, 'update'])->middleware(['auth:sanctum']);
 
-Route::post("/rewards/daily", [RewardController::class, 'store']);
+Route::post("/rewards/daily", [RewardController::class, 'store'])->middleware(['auth:sanctum']);
